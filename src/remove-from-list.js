8057bac5e92c059a59d816ad30ba913799a3867a
Anastasiya <nastya.mikholap@gmail.com>
2022-10-17 16:03:03 +0200

    feat: add task remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,9 +22,89 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+class List {
+
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  add(value) {
+    let node = new ListNode(value);
+    if(this.head === null) {
+      this.head = node;
+    } else {
+      let currentNode = this.head;
+      while(currentNode.next) {
+       currentNode = currentNode.next;
+      }
+      currentNode.next = node;
+    }
+    this.length++;
+  }
+
+  remove(k) {
+    let currentNode = this.head;
+    let previousNode;
+    while(currentNode !== null) {
+      if(this.head.value === k) {
+         this.head = currentNode.next;
+         this.length--;
+         currentNode = this.head;
+       } else {
+           if(currentNode.value === k) {
+             previousNode.next = currentNode.next;
+             currentNode = currentNode.next;
+             this.length--;
+           } else {
+             previousNode = currentNode;
+             currentNode = currentNode.next;
+           }
+         }
+     }
+
+  }
+
+}
+
+// function remove(head, k) {
+//   let currentNode = head;
+//   let previousNode;
+//   while(currentNode !== null) {
+//     if(head.value === k) {
+//        head = currentNode.next;
+//        currentNode = head;
+//      } else {
+//          if(currentNode.value === k) {
+//            previousNode.next = currentNode.next;
+//            currentNode = currentNode.next;
+//          } else {
+//            previousNode = currentNode;
+//            currentNode = currentNode.next;
+//          }
+//        }
+//    }
+// }
+
+function removeKFromList(l, k) {
+  let currentNode = l;
+  let previousNode;
+  while(currentNode !== null) {
+    if(l.value === k) {
+       l = currentNode.next;
+       currentNode = l;
+     } else {
+         if(currentNode.value === k) {
+           previousNode.next = currentNode.next;
+           currentNode = currentNode.next;
+         } else {
+           previousNode = currentNode;
+           currentNode = currentNode.next;
+         }
+       }
+   }
+  return l;
 }
 
 module.exports = {
